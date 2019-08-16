74e987fe17a596c3e4e8955443f55917a53da65b
Umreen <uidigitalcrafts@gmail.com>
2019-08-16 15:17:16 -0500

    git ignore file and database models created

---
+++ models/recipe.js
@@ -0,0 +1,11 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Recipe = sequelize.define('Recipe', {
+    title: DataTypes.STRING,
+    instruction: DataTypes.STRING
+  }, {});
+  Recipe.associate = function(models) {
+    // associations can be defined here
+  };
+  return Recipe;
+};
